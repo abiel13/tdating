2f97b68e9bbc16101f97871e550f71fca88e1a96
abiel13 <dbestabi28@gmail.com>
2024-07-01 20:39:02 +0100

    debug

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,11 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
-
 const f = createUploadthing();
-
-
-
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -14,7 +10,7 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = {}
+      const user = { id: "" };
 
       // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
