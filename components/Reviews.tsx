fae26cbb2e4f3e0d49120cca6561d3156fbc6873
abiel13 <dbestabi28@gmail.com>
2024-07-21 21:51:46 +0100

    redesign homepage

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -8,7 +8,7 @@ const Reviews = () => {
       <h3 className="font-sans font-bold text-3xl">Reviews</h3>
       <div className="bg-_force_blue-500 py-5 w-full px-5 flex justify-between md:flex-row flex-col gap-8">
         {reviews.map((item, i) => (
-          <div className="w-full h-[340px] md:w-[240px] md:h-[300px] bg-white rounded-lg flex flex-col gap-2 px-2 py-2 shadow-lg">
+          <div key={i} className="w-full h-[340px] md:w-[240px] md:h-[300px] bg-white rounded-lg flex flex-col gap-2 px-2 py-2 shadow-lg">
             <div className="relative flex-1 w-full h-[200px]">
               <Image src={item.image} alt={item.userName} fill />
             </div>
