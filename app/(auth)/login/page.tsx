da1abd82c928d5cc9f1976a76aed200b0981c506
abiel13 <dbestabi28@gmail.com>
2024-07-21 18:18:41 +0100

    commit;

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,37 @@
+// pages/telegram-login.js
+
+import Head from 'next/head';
+import Script from 'next/script';
+import styles from '@/styles/telegram-login.module.css';
+
+export default function TelegramLogin() {
+  return (
+    <>
+      <Head>
+        <title>Telegram Login</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className='text-white font-bold text-lg mb-8'>Login To FlirtGram</h1>
+        <div id="telegram-login-container"></div>
+      </div>
+      <Script
+        async
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="flirtgram_bot"
+        data-size="large"
+        data-onauth="onTelegramAuth(user)"
+        data-request-access="write"
+      />
+      <Script
+        id="telegram-auth-handler"
+        strategy="afterInteractive"
+      >
+        {`
+          function onTelegramAuth(user) {
+            alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+          }
+        `}
+      </Script>
+    </>
+  );
+}
--- components/TelegramLoginButton.tsx
+++ components/TelegramLoginButton.tsx
@@ -10,44 +10,41 @@ const TelegramLoginButton = () => {
     console.log(user);
     document.cookie = `tdating-user-data=${JSON.stringify(user)}`;
     alert(user.toString());
-    router.push("/onboarding");
+    // router.push("/onboarding");
   };
 
-  
   return (
-    <div >
-      <Script
-        src="https://telegram.org/js/telegram-widget.js?22"
-        data-telegram-login="flirtgram_bot"
-        data-size="medium"
-        data-onauth="onTelegramAuth(user)"
-        data-request-access="write"
-      ></Script>
-    </div>
+    <Script
+      src="https://telegram.org/js/telegram-widget.js?22"
+      data-telegram-login="flirtgram_bot"
+      data-size="medium"
+      data-onauth="onTelegramAuth(user)"
+      data-request-access="write"
+    />
   );
 };
 
 export default TelegramLoginButton;
 // useEffect(() => {
-  //   window.onTelegramAuth = onTelegramAuth;
+//   window.onTelegramAuth = onTelegramAuth;
 
-  //   const script = document.createElement("script");
-  //   script.src = "https://telegram.org/js/telegram-widget.js?22";
-  //   script.async = true;
-  //   script.setAttribute("data-telegram-login", "testdate12_bot");
-  //   script.setAttribute("data-size", "large");
-  //   script.setAttribute("data-onauth", "onTelegramAuth(user)");
-  //   script.setAttribute("data-request-access", "write");
-  //   const container = document.getElementById("telegram-login-container");
+//   const script = document.createElement("script");
+//   script.src = "https://telegram.org/js/telegram-widget.js?22";
+//   script.async = true;
+//   script.setAttribute("data-telegram-login", "testdate12_bot");
+//   script.setAttribute("data-size", "large");
+//   script.setAttribute("data-onauth", "onTelegramAuth(user)");
+//   script.setAttribute("data-request-access", "write");
+//   const container = document.getElementById("telegram-login-container");
 
-  //   if (container) {
-  //     container.appendChild(script);
-  //   }
+//   if (container) {
+//     container.appendChild(script);
+//   }
 
-  //   return () => {
-  //     if (container) {
-  //       container.removeChild(script);
-  //     }
-  //     window.onTelegramAuth = undefined;
-  //   };
-  // }, [onTelegramAuth]);
+//   return () => {
+//     if (container) {
+//       container.removeChild(script);
+//     }
+//     window.onTelegramAuth = undefined;
+//   };
+// }, [onTelegramAuth]);
