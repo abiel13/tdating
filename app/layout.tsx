4322479090cb0cd5158e6fd6fa46fe9a244a8e9f
abiel13 <dbestabi28@gmail.com>
2024-07-21 02:18:12 +0100

    location searching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TelegramContainer from "@/components/TelegramContainer";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
