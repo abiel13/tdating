8a648faf48b361771a32a8904e7bf5af8086d3f3
abiel13 <dbestabi28@gmail.com>
2024-08-11 17:46:36 +0100

    Initial commit

---
+++ lib/actions/message.actions.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import MessageRequests from "../models/message";
+
+export const createMessageRequest = async (
+  fromUserId: string,
+  toUserId: string,
+  message: string
+) => {
+  try {
+    // Check if a pending message request already exists from the same user to the same recipient
+    const existingRequest = await MessageRequests.findOne({
+      fromUserId,
+      toUserId,
+      status: "Pending",
+    });
+
+    if (existingRequest) {
+      return "Request Had Already Being Sent";
+    }
+
+    // If no duplicate exists, create the new message request
+    const messageRequest = new MessageRequests({
+      fromUserId,
+      toUserId,
+      message,
+    });
+
+    await messageRequest.save();
+    return JSON.parse(JSON.stringify(messageRequest));
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserMessageRequest = async (toUserId: string) => {
+  try {
+    const messageRequests = await MessageRequests.find({ toUserId })
+      .populate("fromUserId")
+      .populate("toUserId");
+    return messageRequests;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateMessageReqStatus = async (id: string) => {
+  try {
+    const messageRequest = await MessageRequests.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+
+    if (!messageRequest) {
+      return "Message Request Not Found Confirm Username";
+    }
+
+    return JSON.parse(JSON.stringify(messageRequest));
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const deleteMessageRequest = async (id:string) => {
+    try {
+        const messageRequest = await MessageRequests.findByIdAndDelete(id);
+
+      
+      
+    } catch (error) {
+        
+    }
+}
--- lib/models/message.ts
+++ lib/models/message.ts
@@ -9,6 +9,12 @@ const messageRequestsSchema = new mongoose.Schema({
           ref: "User",
           required: true,
         },
+        toUserId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+          index: true, // Indexing for faster queries
+        },
         message: {
           type: String,
           required: true,
@@ -17,6 +23,7 @@ const messageRequestsSchema = new mongoose.Schema({
           type: String,
           enum: ["Pending", "Accepted", "Rejected"],
           default: "Pending",
+          index: true,
         },
         requestDate: {
           type: Date,
