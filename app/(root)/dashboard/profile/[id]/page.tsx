19ab23254f942246acb084fb403dfb440f865c19
Abiel asimiea <dbestabi28@gmail.com>
2024-08-21 01:57:57 +0100

    fixing bugs

--- app/(root)/dashboard/profile/[id]/page.tsx
+++ app/(root)/dashboard/profile/[id]/page.tsx
@@ -23,34 +23,6 @@ const UserProfile = () => {
   const [userData, setUserData] = useState<User | null>(null);
   const isMe = id === user?.id;
 
-  async function createDemoUsers() {
-    const data = {
-      fullName: "KTovew we",
-      bio: " A 25-year-old financial analyst from Chicago. He's passionate about cooking gourmet meals and traveling to new countries to experience different cuisines.",
-      dateOfBirth: new Date("2001-01-03T23:00:00.000Z"),
-      username: "de334sas",
-      gender: "Male",
-      profilePictures: [
-        "https://i.pinimg.com/236x/90/05/7c/90057c033320cf6064a60888c591be08.jpg",
-        "https://i.pinimg.com/236x/29/56/e0/2956e0ce8ba5f2d8e64b6ba72a47ef84.jpg",
-        "https://i.pinimg.com/236x/a5/d7/08/a5d708d7d77d7a02a39361ba429236c3.jpg",
-        "https://i.pinimg.com/236x/8e/eb/33/8eeb33e64bd0139d9cd97a0a7148b9e9.jpg",
-      ],
-      interests: ["Dancing", "Singing", "Photography"],
-      telegramChatId: "6578909865",
-      location: {
-        type: "Point",
-        coordinates: [4.8365521, 6.9609754],
-      },
-    };
-
-    try {
-      const response = await createUser({ ...data });
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   useEffect(() => {
     (async function () {
